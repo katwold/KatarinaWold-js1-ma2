9dbd8a7e6bdfed9738178418af54e585616c3913
katwold <61921125+katwold@users.noreply.github.com>
2020-09-20 22:55:17 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+// question 1
+// const button = document.querySelector("#bg");
+// button.onclick = function(){
+// event.target.style.background = "green"
+// }
+//
+//question 2
+// const button = document.querySelector(".togglee");
+// button.onclick = function(){
+// event.target.classList.toggle("extra");
+// }
+//
+//question3
+// const dropdown = document.querySelector("select")
+// const container = document.querySelector("paragraphs-container")
+//
+// dropdown.onchange = function(){
+// const selectedValue = this.value
+//
+// for (let i = 1; i = <=selectedValue; i++){
+//        container.innerHTML += <div class="content"></div>;
+//      }
+//  }
+//
+//question 4
+// const textinput = document.querySelector(".input");
+// const characterCount = document.querySelector(".char-count b");
+// 
+// textinput.onkeyup = function(){
+//     const len = event.target.value.length;
+//     characterCount.innerHTML = len;
+// }
+//
+//question 5
+// window.onscroll = function(){
+//   const scrolledY = window.scrollY;
+//   if (scrolledY)>35{
+//     document.body.classList.add("highlight");
+//     } else {
+//     document.body.classList.remove("highlight");
+//   }
+// }
+     
